600cd8a0c239f0205b64b3d749a63ce8f2978000
AngeloZorzi <astro.drg666@gmail.com>
2025-04-09 17:07:39 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,26 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import CustomNavbar from "./components/Navbar";
 import FilmGallery from "./components/Gallery";
 import Footer from "./components/Footer";
+import { BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <header>
-        <CustomNavbar />
-      </header>
-      <main>
-        <FilmGallery title="STARWARS SAGA" film="Star Wars" />
-        <FilmGallery title="SAW SAGA" film="Saw" />
-        <FilmGallery title="LORD OF THE RINGS" film="Lord Of The Rings" />
-        <FilmGallery title="MARVEL MULTIVERSE" film="Marvel" />
-        <FilmGallery title="ANIME" film="Anime" />
-      </main>
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <header>
+            <CustomNavbar />
+          </header>
+          <main>
+            <FilmGallery title="STARWARS SAGA" film="Star Wars" />
+            <FilmGallery title="SAW SAGA" film="Saw" />
+            <FilmGallery title="LORD OF THE RINGS" film="Lord Of The Rings" />
+            <FilmGallery title="MARVEL MULTIVERSE" film="Marvel" />
+            <FilmGallery title="ANIME" film="Anime" />
+          </main>
+          <Footer />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
